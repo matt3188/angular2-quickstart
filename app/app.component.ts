f6165a3385592f41888c2453885874ccdafdd1e2
Matt Coleman <hello@matt-coleman.co.uk>
2016-03-11 17:35:36 +0000

    Created hero.service file and imported it where necessary

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,7 @@
-import {Component} from 'angular2/core';
+import {Component, OnInit} from 'angular2/core';
 import {Hero} from './hero';
 import {HeroDetailComponent} from './hero-detail.component';
-
+import {HeroService} from './hero.service';
 @Component({
   selector: 'my-app',
   template:`
@@ -65,11 +65,19 @@ import {HeroDetailComponent} from './hero-detail.component';
       border-radius: 4px 0 0 4px;
     }
   `],
-  directives: [HeroDetailComponent]
+  directives: [HeroDetailComponent],
+  providers: [HeroService]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Tour of Heroes';
-  heroes = HEROES;
+  heroes: Hero[];
   selectedHero: Hero;
+  constructor(private _heroService: HeroService) { }
+  getHeroes() {
+    this._heroService.getHeroes().then(heroes => this.heroes = heroes);
+  }
+  ngOnInit() {
+    this.getHeroes();
+  }
   onSelect(hero: Hero) { this.selectedHero = hero; }
 }
--- app/hero-detail.component.ts
+++ app/hero-detail.component.ts
@@ -1,10 +1,9 @@
 import {Component} from 'angular2/core';
 import {Hero} from './hero';
-
 @Component({
   selector: 'my-hero-detail',
-  inputs: ['hero'],
-  template: `<div *ngIf="hero">
+  template: `
+    <div *ngIf="hero">
       <h2>{{hero.name}} details!</h2>
       <div><label>id: </label>{{hero.id}}</div>
       <div>
@@ -13,6 +12,7 @@ import {Hero} from './hero';
       </div>
     </div>
   `,
+  inputs: ['hero']
 })
 export class HeroDetailComponent {
   hero: Hero;
