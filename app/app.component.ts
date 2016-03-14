5913921382b3c06c22be8018f950a9cb87907191
Matt Coleman <hello@matt-coleman.co.uk>
2016-03-14 17:08:53 +0000

    Added router.dev library and update app.component

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,14 +1,17 @@
 import { Component }       from 'angular2/core';
 import { HeroService }     from './hero.service';
 import { HeroesComponent } from './heroes.component';
+import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from 'angular2/router';
+
 @Component({
   selector: 'my-app',
   template: `
     <h1>{{title}}</h1>
     <my-heroes></my-heroes>
   `,
-  directives: [HeroesComponent],
+  directives: [ROUTER_DIRECTIVES],
   providers: [
+    ROUTER_PROVIDERS,
     HeroService
   ]
 })
