696de195cc30cbb7c9895a8860635c7004e1cd7e
Matt Coleman <hello@matt-coleman.co.uk>
2016-03-14 17:50:32 +0000

    Added dashboard logic

--- app/dashboard.component.ts
+++ app/dashboard.component.ts
@@ -11,4 +11,16 @@ import { HeroService } from './hero.service';
   templateUrl: 'app/dashboard.component.html'
 })
 export class DashboardComponent implements OnInit {
+
+  heroes: Hero[] = [];
+
+  constructor( private _heroService: HeroService ) { }
+
+  ngOnInit() {
+    this._heroService.getHeroes()
+      .then(heroes => this.heroes = heroes.slice(1, 5));
+  }
+
+  gotoDetail(){ /* not implemented yet */ }
+  
 }
